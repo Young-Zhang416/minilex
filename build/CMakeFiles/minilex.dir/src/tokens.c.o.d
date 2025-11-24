89f0db42417e7ab9e36b251baf07556a6be1f6a6
root <zhangyang>
2025-11-24 17:14:03 +0800

    Untrack .vscode build and target directory

--- build/CMakeFiles/minilex.dir/src/tokens.c.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/minilex.dir/src/tokens.c.o: /root/minilex/src/tokens.c \
- /usr/include/stdc-predef.h \
- /usr/lib/gcc/x86_64-linux-gnu/13/include/stddef.h \
- /root/minilex/include/tokens.h
